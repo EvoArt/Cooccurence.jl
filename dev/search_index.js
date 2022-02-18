0929fa3d838efcb6fe8ab43c579a02923f594754
Documenter.jl <documenter@juliadocs.github.io>
2022-02-18 11:50:37 +0000

    build based on 48f8fd5

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Cooccurence","category":"page"},{"location":"#Cooccurence","page":"Home","title":"Cooccurence","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Cooccurence.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Cooccurence]","category":"page"}]
+}
